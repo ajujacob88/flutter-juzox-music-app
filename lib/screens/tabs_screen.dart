eedd197087bc4cca6709532574545260192e8ea6
ajujacob <ajujacob88@gmail.com>
2024-05-08 11:25:55 +0530

    changing the bottom navigation bar theme

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<TabsScreen> {
 
     return Scaffold(
       backgroundColor: Colors.transparent,
-      //extendBody: true,
+      extendBody: true,
       // appBar: AppBar(
       //   backgroundColor: Colors.transparent,
       //   title: const Text('My Music'),
--- lib/widgets/juzox_bottom_navigation_bar.dart
+++ lib/widgets/juzox_bottom_navigation_bar.dart
@@ -15,14 +15,112 @@ class _JuzoxBottomNavigationBarState extends State<JuzoxBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: const BorderRadius.only(
-        topLeft: Radius.circular(75),
-        topRight: Radius.circular(75),
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        //padding: EdgeInsets.only(left: 20, right: 20),
+        decoration: const BoxDecoration(
+          color: Color.fromARGB(255, 1, 0, 6),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(80),
+            topRight: Radius.circular(80),
+          ),
+        ),
+        child: Container(
+          height: 70,
+          decoration: const BoxDecoration(
+            color: Colors.amber,
+            // backgroundBlendMode: BlendMode.color,
+            borderRadius: BorderRadius.only(
+              // topLeft: Radius.circular(80),
+              // topRight: Radius.circular(80),
+              topLeft: Radius.elliptical(80, 80),
+              topRight: Radius.elliptical(80, 80),
+            ),
+            gradient: LinearGradient(
+              colors: [
+                // Color.fromARGB(158, 105, 240, 175),
+
+                Color.fromARGB(127, 5, 37, 73),
+                Color.fromARGB(129, 64, 195, 255),
+              ], // Adjust colors as needed
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight, // Adjust gradient direction as needed
+            ),
+          ),
+          child: NavigationBar(
+            onDestinationSelected: (index) {
+              setState(() {
+                currentPageIndex = index;
+                widget.onCurrentPageChanged(currentPageIndex);
+              });
+            },
+            // labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            // backgroundColor: const Color.fromARGB(44, 0, 0, 0),
+            //backgroundColor: Color.fromARGB(105, 0, 20, 27),
+            //backgroundColor: Color.fromARGB(22, 68, 137, 255),
+
+            //backgroundColor: Color.fromARGB(255, 3, 0, 35),
+
+            backgroundColor: Colors.transparent,
+            // indicatorColor: Colors.amber,
+            //height: 70,
+            elevation: 0,
+            selectedIndex: currentPageIndex,
+            destinations: const [
+              NavigationDestination(
+                selectedIcon: Icon(Icons.home),
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.music_note),
+                icon: Icon(Icons.music_note_outlined),
+                label: 'Music',
+              ),
+              NavigationDestination(
+                // selectedIcon: Icon(Icons.library_music),
+                icon: Icon(Icons.library_music),
+                label: 'Library',
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+
+
+/*
+import 'package:flutter/material.dart';
+
+class JuzoxBottomNavigationBar extends StatefulWidget {
+  const JuzoxBottomNavigationBar(
+      {super.key, required this.onCurrentPageChanged});
+
+  final Function(int) onCurrentPageChanged;
+  @override
+  State<JuzoxBottomNavigationBar> createState() =>
+      _JuzoxBottomNavigationBarState();
+}
+
+class _JuzoxBottomNavigationBarState extends State<JuzoxBottomNavigationBar> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 70,
+      color: Colors.black,
       child: Container(
         height: 70,
+        // color: Colors.black,
         decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(200), topRight: Radius.circular(200),
+              ),
           gradient: LinearGradient(
             colors: [
               // Color.fromARGB(158, 105, 240, 175),
@@ -56,7 +154,10 @@ class _JuzoxBottomNavigationBarState extends State<JuzoxBottomNavigationBar> {
           destinations: const [
             NavigationDestination(
               selectedIcon: Icon(Icons.home),
-              icon: Icon(Icons.home_outlined),
+              icon: Icon(
+                Icons.home_outlined,
+                color: Colors.white,
+              ),
               label: 'Home',
             ),
             NavigationDestination(
@@ -77,12 +178,12 @@ class _JuzoxBottomNavigationBarState extends State<JuzoxBottomNavigationBar> {
 }
 
 
+*/
+
 
 /*
-//using material instead of clipRRect, but clipRRect is more efficient
-
+//using cliprrect
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 class JuzoxBottomNavigationBar extends StatefulWidget {
   const JuzoxBottomNavigationBar(
@@ -99,41 +200,69 @@ class _JuzoxBottomNavigationBarState extends State<JuzoxBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Color.fromARGB(223, 2, 133, 98),
-      elevation: 0.0,
-      shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(50))),
-      child: NavigationBar(
-        onDestinationSelected: (index) {
-          setState(() {
-            currentPageIndex = index;
-            widget.onCurrentPageChanged(currentPageIndex);
-          });
-        },
-        // labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
-        backgroundColor: Colors.transparent,
-        // indicatorColor: Colors.amber,
-        //height: 70,
-        elevation: 0,
-        selectedIndex: currentPageIndex,
-        destinations: const [
-          NavigationDestination(
-            selectedIcon: Icon(Icons.home),
-            icon: Icon(Icons.home_outlined),
-            label: 'Home',
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(75),
+        topRight: Radius.circular(75),
+      ),
+      child: Container(
+        color: Color.fromARGB(255, 1, 0, 6),
+        child: Container(
+          height: 70,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(200),
+              topRight: Radius.circular(200),
+            ),
+            gradient: LinearGradient(
+              colors: [
+                // Color.fromARGB(158, 105, 240, 175),
+
+                Color.fromARGB(127, 5, 37, 73),
+                Color.fromARGB(129, 64, 195, 255),
+              ], // Adjust colors as needed
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight, // Adjust gradient direction as needed
+            ),
           ),
-          NavigationDestination(
-            // selectedIcon: Icon(Icons.search),
-            icon: Icon(Icons.search_outlined),
-            label: 'Search',
+          child: NavigationBar(
+            onDestinationSelected: (index) {
+              setState(() {
+                currentPageIndex = index;
+                widget.onCurrentPageChanged(currentPageIndex);
+              });
+            },
+            // labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
+            // backgroundColor: const Color.fromARGB(44, 0, 0, 0),
+            //backgroundColor: Color.fromARGB(105, 0, 20, 27),
+            //backgroundColor: Color.fromARGB(22, 68, 137, 255),
+
+            //backgroundColor: Color.fromARGB(255, 3, 0, 35),
+
+            backgroundColor: Colors.transparent,
+            // indicatorColor: Colors.amber,
+            //height: 70,
+            elevation: 0,
+            selectedIndex: currentPageIndex,
+            destinations: const [
+              NavigationDestination(
+                selectedIcon: Icon(Icons.home),
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                selectedIcon: Icon(Icons.music_note),
+                icon: Icon(Icons.music_note_outlined),
+                label: 'Music',
+              ),
+              NavigationDestination(
+                // selectedIcon: Icon(Icons.library_music),
+                icon: Icon(Icons.library_music),
+                label: 'Library',
+              ),
+            ],
           ),
-          NavigationDestination(
-            // selectedIcon: Icon(Icons.library_music),
-            icon: Icon(Icons.library_music),
-            label: 'Library',
-          ),
-        ],
+        ),
       ),
     );
   }
