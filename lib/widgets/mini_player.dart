2384f1bca34669da9694795ec5b75905b984bc73
ajujacob <ajujacob88@gmail.com>
2024-05-30 16:39:21 +0530

    designing mini player. adding music image to miniplayer

--- lib/widgets/mini_player.dart
+++ lib/widgets/mini_player.dart
@@ -25,6 +25,8 @@ class _MiniPlayerState extends State<MiniPlayer> {
   Duration currentDuration = Duration.zero;
   Duration totalDuration = Duration.zero;
 
+  final OnAudioQuery _audioQuery = OnAudioQuery();
+
   @override
   void initState() {
     super.initState();
@@ -85,107 +87,138 @@ class _MiniPlayerState extends State<MiniPlayer> {
         color: Color.fromARGB(255, 1, 33, 47),
       ),
       padding: const EdgeInsets.all(0.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
+      child: Row(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.skip_previous, color: Colors.white),
-                onPressed: () {},
-                constraints:
-                    BoxConstraints(), // Remove constraints to minimize the size
-                padding: EdgeInsets.all(0),
-              ),
-              IconButton(
-                icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow,
-                    color: Colors.white),
-                constraints:
-                    BoxConstraints(), // Remove constraints to minimize the size
-                padding: EdgeInsets.zero,
+          QueryArtworkWidget(
+            id: widget.song.id!,
+            type: ArtworkType.AUDIO,
+            // controller: _audioQuery,
+            artworkBorder: const BorderRadius.horizontal(
+                left: Radius.circular(8), right: Radius.circular(8)),
+            artworkClipBehavior: Clip.hardEdge,
+            nullArtworkWidget: Container(
+              decoration: const BoxDecoration(
+                  color: Color.fromARGB(22, 68, 137, 255),
+                  borderRadius: BorderRadius.horizontal(
+                      left: Radius.circular(8), right: Radius.circular(8))),
+              // Set desired width and height for the box
+              width: 50.0, // Adjust as needed
+              height: 50.0, // Adjust as needed
 
-                onPressed: () {
-                  if (isPlaying) {
-                    widget.juzoxAudioPlayerService.juzoxPause();
-                  } else {
-                    widget.juzoxAudioPlayerService
-                        .juzoxPlay(widget.song.filePath);
-                  }
-                },
+              child: const Icon(
+                Icons.music_note_outlined,
+                color: Color.fromARGB(140, 64, 195, 255),
+                size: 30,
               ),
-              IconButton(
-                icon: const Icon(Icons.skip_next, color: Colors.white),
-                onPressed: () {},
-                constraints:
-                    BoxConstraints(), // Remove constraints to minimize the size
-                padding: EdgeInsets.zero,
-              ),
-              Expanded(
-                child: SizedBox(
-                  height: 24,
-                  child: Marquee(
-                    key: ValueKey(widget.song.filePath),
-                    // Unique key based on the song's file path. To ensure that the Marquee always starts with the song title starting position when you click a song, you can utilize the key property of the Marquee widget. By changing the key whenever the song changes, the Marquee will reset and start from the beginning.
-                    text: songDisplayText,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                    //textScaleFactor: 1.3,
-                    // blankSpace: 40.0,
-                    // velocity: 30,
-
-                    blankSpace: 40.0,
-                    velocity: 20,
-                    pauseAfterRound: Duration(seconds: 1),
-                    startPadding: 10.0,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-          SizedBox(
-            height: 20,
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 10.0),
-              child: SliderTheme(
-                data: SliderTheme.of(context).copyWith(
-                  trackHeight: 0.8,
-                  trackShape: const RoundedRectSliderTrackShape(),
-                  //  activeTrackColor: Colors.purple.shade800,
-                  //  inactiveTrackColor: Colors.purple.shade100,
-                  thumbShape: const RoundSliderThumbShape(
-                    enabledThumbRadius: 8.0,
-                    pressedElevation: 20.0,
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon:
+                          const Icon(Icons.skip_previous, color: Colors.white),
+                      onPressed: () {},
+                      constraints:
+                          BoxConstraints(), // Remove constraints to minimize the size
+                      padding: EdgeInsets.all(0),
+                    ),
+                    IconButton(
+                      icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow,
+                          color: Colors.white),
+                      constraints:
+                          BoxConstraints(), // Remove constraints to minimize the size
+                      padding: EdgeInsets.zero,
+
+                      onPressed: () {
+                        if (isPlaying) {
+                          widget.juzoxAudioPlayerService.juzoxPause();
+                        } else {
+                          widget.juzoxAudioPlayerService
+                              .juzoxPlay(widget.song.filePath);
+                        }
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.skip_next, color: Colors.white),
+                      onPressed: () {},
+                      constraints:
+                          BoxConstraints(), // Remove constraints to minimize the size
+                      padding: EdgeInsets.zero,
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 24,
+                        child: Marquee(
+                          key: ValueKey(widget.song.filePath),
+                          // Unique key based on the song's file path. To ensure that the Marquee always starts with the song title starting position when you click a song, you can utilize the key property of the Marquee widget. By changing the key whenever the song changes, the Marquee will reset and start from the beginning.
+                          text: songDisplayText,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                          //textScaleFactor: 1.3,
+                          // blankSpace: 40.0,
+                          // velocity: 30,
+
+                          blankSpace: 40.0,
+                          velocity: 20,
+                          pauseAfterRound: Duration(seconds: 1),
+                          startPadding: 10.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        trackHeight: 0.8,
+                        trackShape: const RoundedRectSliderTrackShape(),
+                        //  activeTrackColor: Colors.purple.shade800,
+                        //  inactiveTrackColor: Colors.purple.shade100,
+                        thumbShape: const RoundSliderThumbShape(
+                          enabledThumbRadius: 8.0,
+                          pressedElevation: 20.0,
+                        ),
+                        //     thumbColor: Colors.pinkAccent,
+                        //     overlayColor: Colors.pink.withOpacity(0.2),
+                        overlayShape:
+                            const RoundSliderOverlayShape(overlayRadius: 22.0),
+                        tickMarkShape: const RoundSliderTickMarkShape(),
+                        //     activeTickMarkColor: Colors.pinkAccent,
+                        //   inactiveTickMarkColor: Colors.white,
+                        valueIndicatorShape:
+                            const PaddleSliderValueIndicatorShape(),
+                        valueIndicatorColor: Colors.black,
+                        // valueIndicatorTextStyle: const TextStyle(
+                        //   color: Colors.white,
+                        //   fontSize: 20.0,
+                        // ),
+                      ),
+                      child: Slider(
+                        activeColor: const Color.fromARGB(193, 64, 195, 255),
+                        thumbColor: Colors.lightBlueAccent,
+                        inactiveColor: const Color.fromARGB(94, 64, 195, 255),
+                        value: currentDuration.inSeconds.toDouble(),
+                        max: totalDuration.inSeconds.toDouble(),
+                        onChanged: (value) {
+                          widget.juzoxAudioPlayerService.audioPlayer
+                              .seek(Duration(seconds: value.toInt()));
+                        },
+                      ),
+                    ),
                   ),
-                  //     thumbColor: Colors.pinkAccent,
-                  //     overlayColor: Colors.pink.withOpacity(0.2),
-                  overlayShape:
-                      const RoundSliderOverlayShape(overlayRadius: 22.0),
-                  tickMarkShape: const RoundSliderTickMarkShape(),
-                  //     activeTickMarkColor: Colors.pinkAccent,
-                  //   inactiveTickMarkColor: Colors.white,
-                  valueIndicatorShape: const PaddleSliderValueIndicatorShape(),
-                  valueIndicatorColor: Colors.black,
-                  // valueIndicatorTextStyle: const TextStyle(
-                  //   color: Colors.white,
-                  //   fontSize: 20.0,
-                  // ),
                 ),
-                child: Slider(
-                  activeColor: const Color.fromARGB(193, 64, 195, 255),
-                  thumbColor: Colors.lightBlueAccent,
-                  inactiveColor: const Color.fromARGB(94, 64, 195, 255),
-                  value: currentDuration.inSeconds.toDouble(),
-                  max: totalDuration.inSeconds.toDouble(),
-                  onChanged: (value) {
-                    widget.juzoxAudioPlayerService.audioPlayer
-                        .seek(Duration(seconds: value.toInt()));
-                  },
-                ),
-              ),
+              ],
             ),
           ),
         ],
