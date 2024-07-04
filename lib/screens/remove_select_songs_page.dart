6f32d363d81ec700a6840cc3511b4facfa7b3bcd
ajujacob <ajujacob88@gmail.com>
2024-07-04 19:03:30 +0530

    playlist tab - remove songs page - modified and completed

--- lib/screens/remove_select_songs_page.dart
+++ lib/screens/remove_select_songs_page.dart
@@ -14,6 +14,92 @@ class RemoveSelectSongsPage extends StatefulWidget {
 
 class _RemoveSelectSongsPageState extends State<RemoveSelectSongsPage> {
   List<JuzoxMusicModel> _selectedSongs = [];
+  Set<JuzoxMusicModel> _markedForRemoval = {};
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize _selectedSongs with the songs already in the playlist
+    final existingSongs =
+        Provider.of<AudioPlayerProvider>(context, listen: false)
+                .userPlaylistSongs[widget.playlistName] ??
+            [];
+    _selectedSongs = List.from(existingSongs);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Remove Songs from "${widget.playlistName}"'),
+      ),
+      body: ListView.builder(
+        itemCount: _selectedSongs.length,
+        itemBuilder: (context, index) {
+          final song = _selectedSongs[index];
+          final isMarkedForRemoval = _markedForRemoval.contains(song);
+          return ListTile(
+            title: Text(song.title ??
+                'Unknown Title'), // Adjust based on your music model
+            trailing: Icon(
+              isMarkedForRemoval
+                  // ? Icons.check_box_outline_blank
+                  // : Icons.check_box,
+                  ? Icons.remove_circle_outlined
+                  : Icons.remove_circle_outline,
+              // color: isMarkedForRemoval ? null : Colors.red,
+              color: isMarkedForRemoval ? Colors.red : null,
+            ),
+            onTap: () {
+              setState(() {
+                if (isMarkedForRemoval) {
+                  _markedForRemoval.remove(song);
+                } else {
+                  _markedForRemoval.add(song);
+                }
+              });
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Remove marked songs from the playlist
+          _selectedSongs
+              .removeWhere((song) => _markedForRemoval.contains(song));
+
+          // Update the provider with the new playlist
+          Provider.of<AudioPlayerProvider>(context, listen: false)
+              .addSongsToPlaylist(widget.playlistName, _selectedSongs);
+
+          Navigator.of(context).pop();
+        },
+        child: const Icon(Icons.done),
+      ),
+    );
+  }
+}
+
+
+
+/*
+import 'package:flutter/material.dart';
+import 'package:juzox_music_app/models/music_model.dart';
+import 'package:juzox_music_app/providers/audio_player_provider.dart';
+import 'package:provider/provider.dart';
+
+class RemoveSelectSongsPage extends StatefulWidget {
+  final String playlistName;
+
+  const RemoveSelectSongsPage({super.key, required this.playlistName});
+
+  @override
+  State<RemoveSelectSongsPage> createState() => _RemoveSelectSongsPageState();
+}
+
+class _RemoveSelectSongsPageState extends State<RemoveSelectSongsPage> {
+  List<JuzoxMusicModel> _selectedSongs = [];
 
   @override
   void initState() {
@@ -91,3 +177,4 @@ class _RemoveSelectSongsPageState extends State<RemoveSelectSongsPage> {
     );
   }
 }
+*/
