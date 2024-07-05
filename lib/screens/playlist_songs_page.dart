2fc6ad712944261c9042c36c99cbf47878870625
ajujacob <ajujacob88@gmail.com>
2024-07-05 11:15:41 +0530

    playlist songs page - designing

--- lib/screens/playlist_songs_page.dart
+++ lib/screens/playlist_songs_page.dart
@@ -386,7 +386,7 @@ class PlaylistSongsPage extends StatelessWidget {
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 100),
                   child: Text(
-                    'This much $playlistName',
+                    'This much songs in $playlistName',
                   ),
                 )),
           ),
